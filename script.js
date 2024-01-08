85860eb19e5cb46584419e443e0fc895d7787b2c
gayan65728@yahoo.com <egawi001@edu.xamk.fi>
2024-01-08 12:59:29 +0200

    Add toggle function tho the button

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 document.getElementById("actionButton").addEventListener("click", function () {
-    alert("Button clicked!");
+    // alert("Button clicked!");
+    var paragraph = document.getElementById("description");
+    // Calling the function
+    toggleVisibility(paragraph);
 });
 
 // Get the current year
@@ -7,3 +10,12 @@ var currentYear = new Date().getFullYear();
 
 // Update the footer with the current year
 document.getElementById("currentYear").innerText = currentYear;
+
+//Function for the toggle visibility
+function toggleVisibility(paragraph) {
+    if (window.getComputedStyle(paragraph).display === "none") {
+        paragraph.style.display = "block";
+    } else {
+        paragraph.style.display = "none";
+    }
+}
